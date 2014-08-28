235c953a5d98f6fd67c4496ea8682d4e108e9a4d
Stephen Chang <stchang@ccs.neu.edu>
2014-08-28 15:22:42 -0400

    add stlc+define+cons-via-racket-extended.rkt

--- stlc-tests.rkt
+++ stlc-tests.rkt
@@ -1,5 +1,16 @@
-#lang s-exp "stlc.rkt";"stlc-via-racket-extended.rkt"
+#lang s-exp "stlc+define+cons-via-racket-extended.rkt"
 ((λ ([f : (Int → Int)] [x : Int]) (f x))
    (λ ([x : Int]) (+ x x 1))
    10)
 ((λ ([x : Int]) (+ x 1 3)) 100)
+
+;; extra tests
+; test #%datum extension
+
+; when lang is stlc: should fail with: "dont know type for literal" (but not inf loop)
+; when lang is stlc+define+cons: should be ok
+;#f 
+
+;; lang: stlc: fail
+;; lang: stlc+define: fail
+;1.2
