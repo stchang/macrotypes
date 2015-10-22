f7635af9b59d846e10f58102059acf41a328e58b
Stephen Chang <stchang@ccs.neu.edu>
2015-10-22 13:22:35 -0400

    add stlc fail tests

--- tapl/tests/stlc-tests.rkt
+++ tapl/tests/stlc-tests.rkt
@@ -1,4 +1,12 @@
 #lang s-exp "../stlc.rkt"
 (require "rackunit-typechecking.rkt")
 
-;; cannot write any terms without base types
+;; cannot write any terms without base types, but can check some errors
+
+(typecheck-fail (λ ([x : Undef]) x) #:with-msg "Undef: unbound identifier")
+(typecheck-fail (λ ([x : →]) x)
+ #:with-msg "Improper usage of type constructor →.+expected >= 1 arguments")
+(typecheck-fail (λ ([x : (→)]) x)
+ #:with-msg "Improper usage of type constructor →.+expected >= 1 arguments")
+(typecheck-fail (λ ([x : (→ →)]) x)
+ #:with-msg "Improper usage of type constructor →.+expected >= 1 arguments")
