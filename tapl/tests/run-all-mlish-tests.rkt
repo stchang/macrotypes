9308677b2abcc2383e3826a74ce59e76de9ae0a3
Stephen Chang <stchang@ccs.neu.edu>
2016-05-03 14:40:22 -0400

    run-all-mlish-tests script: use find-system-path

--- tapl/tests/run-all-mlish-tests.rkt
+++ tapl/tests/run-all-mlish-tests.rkt
@@ -1,11 +1,8 @@
 #lang racket/base
 (require (for-syntax racket/base syntax/parse racket/syntax syntax/stx))
-(require racket/match racket/system racket/port setup/dirs racket/format)
+(require racket/match racket/system racket/port racket/format)
 
-(define PLT-ROOT/REV (cdr (reverse (explode-path (find-collects-dir)))))
-(define BIN "bin")
-(define R 
-  (path->string (apply build-path (reverse `("racket" ,BIN . ,PLT-ROOT/REV)))))
+(define R (path->string (find-system-path 'exec-file)))
 
 (define (mk-process-cmd r n)
   (string-append "time " r " run-mlish-tests" (number->string n) ".rkt"))
@@ -13,18 +10,7 @@
 (define-for-syntax ((mk-num-id str) n-stx)
   (format-id n-stx (string-append str "~a") (syntax-e n-stx)))
 
-(define-syntax (start stx)
-  (syntax-parse stx
-    [(_ n)
-     #:with in (mk-num-id "i" #'n)
-     #:with out (mk-num-id "o" #'n)
-     #:with id (mk-num-id "id" #'n)
-     #:with err (mk-num-id "err" #'n)
-     #:with f (mk-num-id "f" #'n)
-     #'(match-define (list in out id err f)
-         (process (mk-process R n)))]))
-
-;; test: abstracts and interleaves the following def, reporting, and cleanup:
+;; do-test: abstracts and interleaves the following def, reporting, and cleanup:
 ;;  (match-define (list i1 o1 id1 err1 f1)
 ;;    (process "time racket run-mlish-tests1.rkt"))
 ;;  (displayln "---- tests: General MLish tests: -----------------------------")
