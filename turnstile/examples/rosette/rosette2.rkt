add4a2d7d5b09688350b195a095c5a6243277634
AlexKnauth <alexander@knauth.org>
2016-08-24 19:42:03 -0400

    more work on rosette2

--- turnstile/examples/rosette/rosette2.rkt
+++ turnstile/examples/rosette/rosette2.rkt
@@ -1,9 +1,25 @@
 #lang turnstile
 (extends "../stlc.rkt")
+(reuse #%datum #:from "../stlc+union.rkt")
+(reuse define-type-alias #:from "../stlc+reco+var.rkt")
+(reuse define-named-type-alias #:from "../stlc+union.rkt")
+
+(provide CU U
+         CNegInt NegInt
+         CZero Zero
+         CPosInt PosInt
+         CNat Nat
+         CInt Int
+         CFloat Float
+         CNum Num
+         CBool Bool
+         CString ; symbolic string are not supported
+         )
 
 (require 
- (only-in "../stlc+union.rkt" prune+sort current-sub?)
- (prefix-in C (only-in "../stlc+union+case.rkt" PosInt Zero NegInt Float Bool [U U*] case-> →))
+ (only-in "../stlc+union.rkt" define-named-type-alias prune+sort current-sub?)
+ (prefix-in C (only-in "../stlc+union+case.rkt"
+                       PosInt Zero NegInt Float Bool String [U U*] U*? case-> →))
  (only-in "../stlc+union+case.rkt" [~U* ~CU*] [~case-> ~Ccase->] [~→ ~C→]))
 
 (define-syntax (CU stx)
@@ -28,6 +44,32 @@
   (define (concrete? t)
     (not (U*? t))))
 
+;; ---------------------------------
+;; Symbolic versions of types
+
+(define-named-type-alias NegInt (U CNegInt))
+(define-named-type-alias Zero (U CZero))
+(define-named-type-alias PosInt (U CPosInt))
+(define-named-type-alias Float (U CFloat))
+(define-named-type-alias Bool (U CBool))
+
+(define-syntax define-symbolic-named-type-alias
+  (syntax-parser
+    [(_ Name:id Cτ:expr)
+     #:with Cτ+ ((current-type-eval) #'Cτ)
+     #:fail-when (and (not (concrete? #'Cτ+)) #'Cτ+)
+                 "should be a concrete type"
+     #:with CName (format-id #'Name "C~a" #'Name #:source #'Name)
+     #'(begin-
+         (define-named-type-alias CName Cτ)
+         (define-named-type-alias Name (U CName)))]))
+
+(define-symbolic-named-type-alias Nat (CU CZero CPosInt))
+(define-symbolic-named-type-alias Int (CU CNegInt CNat))
+(define-symbolic-named-type-alias Num (CU CFloat CInt))
+
+;; ---------------------------------
+
 (begin-for-syntax
   (define (sub? t1 t2)
     ; need this because recursive calls made with unexpanded types
@@ -42,8 +84,18 @@
        [((~CU* . ts1) _)
         (for/and ([t (stx->list #'ts1)])
           ((current-sub?) t t2))]
+       [((~U* . ts1) _)
+        (and
+         (not (concrete? t2))
+         (for/and ([t (stx->list #'ts1)])
+           ((current-sub?) t t2)))]
        ; 1 U type, 1 non-U type. subtype = member
        [(_ (~CU* . ts2))
+        #:when (not (or (U*? t1) (CU*? t1)))
+        (for/or ([t (stx->list #'ts2)])
+          ((current-sub?) t1 t))]
+       [(_ (~U* . ts2))
+        #:when (not (or (U*? t1) (CU*? t1)))
         (for/or ([t (stx->list #'ts2)])
           ((current-sub?) t1 t))]
        ; 2 case-> types, subtype = subset
