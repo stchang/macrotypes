d49721a827df73518c3e47e5b5568e4b0990b745
Ben Greenman <benjaminlgreenman@gmail.com>
2016-09-29 14:20:08 -0400

    [artifact] use turnstile logo as background

--- artifact/root-setup.sh
+++ artifact/root-setup.sh
@@ -29,4 +29,4 @@ sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo ALL=(ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
 
 # Install wallpaper
-wget -O /usr/share/backgrounds/xfce/xfce-blue.jpg http://ccs.neu.edu/home/types/racket-logo-silver.jpg
+wget -O /usr/share/backgrounds/xfce/xfce-blue.jpg http://ccs.neu.edu/home/stchang/popl2017/logo.png
