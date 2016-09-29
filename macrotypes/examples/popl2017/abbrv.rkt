995a60251f0085ac18e0cdc4847ca6c7a03fb7f7
Stephen Chang <stchang@ccs.neu.edu>
2016-09-29 19:18:52 -0400

    paper examples: use stx-utils in abbrv.rkt; dont redefine fns

--- macrotypes/examples/popl2017/abbrv.rkt
+++ macrotypes/examples/popl2017/abbrv.rkt
@@ -1,10 +1,12 @@
 #lang racket
 (require syntax/parse/define
-         (for-syntax syntax/stx))
+         (for-syntax "../../stx-utils.rkt" syntax/stx))
 (provide (all-defined-out)
-         (for-syntax (all-defined-out)))
+         (for-syntax (all-defined-out)
+                     (all-from-out "../../stx-utils.rkt")))
 
-;; defines abbreviations used in the paper
+
+;; this file defines and provies abbreviations used in the paper
 
 (define-syntax-parser define-m
   [(_ name:id opt ... not-template)
@@ -22,18 +24,13 @@
     (syntax-property e key (local-expand val 'expression null)))
   (define (get-stx-prop e key) (syntax-property e key))
 
-  (define (stx-length= s1 s2)
-    (= (length (stx->list s1)) (length (stx->list s2))))
-  (define (stx-andmap p? . stxs)
-    (apply andmap p? (map stx->list stxs)))
-
   (define (stx= t1 t2)
     (or (and (identifier? t1) (identifier? t2) (free-identifier=? t1 t2))
         (and (stx-null? t1) (stx-null? t2))
         (and (stx-pair? t1) (stx-pair? t2)
              (stxs= t1 t2))))
   (define (stxs= τs1 τs2)
-    (and (stx-length= τs1 τs2)
+    (and (stx-length=? τs1 τs2)
          (stx-andmap stx= τs1 τs2)))
   (define (τs= τs1 τs2) (stxs= τs1 τs2))
 
