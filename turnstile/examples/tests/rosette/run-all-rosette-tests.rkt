593f4f58810d8c9d1fd51e04dd2f32536898d116
Stephen Chang <stchang@ccs.neu.edu>
2016-10-31 12:26:39 -0400

    fix merge remnant

--- turnstile/examples/tests/rosette/run-all-rosette-tests.rkt
+++ turnstile/examples/tests/rosette/run-all-rosette-tests.rkt
@@ -8,11 +8,7 @@
 ;(require "bv-ref-tests.rkt")
 ; visit but dont instantiate, o.w. will get unsat
 ;(dynamic-require "fsm-test.rkt" #f)
-<<<<<<< HEAD
 (require "ifc-tests.rkt")
 
 ; don't run this file for testing:
 (module test racket/base)
-=======
-;(require "ifc-tests.rkt")
->>>>>>> add optimize, remaining sec3 tests; fix CList-CListof subtyping
