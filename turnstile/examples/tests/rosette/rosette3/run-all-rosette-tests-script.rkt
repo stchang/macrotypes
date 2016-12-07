f2c6b581fe289163c3497790dc195a8d0129350c
Stephen Chang <stchang@ccs.neu.edu>
2016-12-07 16:16:58 -0500

    add walsh transform verify tests

--- turnstile/examples/tests/rosette/rosette3/run-all-rosette-tests-script.rkt
+++ turnstile/examples/tests/rosette/rosette3/run-all-rosette-tests-script.rkt
@@ -25,6 +25,7 @@
           "synthcl3-matrix-synth-tests.rkt" "SynthCL Matrix Mult: synth"
           "synthcl3-matrix-verify-tests.rkt" "SynthCL Matrix Mult: verify"
           "synthcl3-matrix-verify-buggy-tests.rkt" "SynthCL buggy Matrix Mult: verify"
-          "synthcl3-walsh-synth-tests.rkt" "SynthCL Walsh Transform: synth")
+          "synthcl3-walsh-synth-tests.rkt" "SynthCL Walsh Transform: synth"
+          "synthcl3-walsh-verify-tests.rkt" "SynthCL Walsh Transform: verify")
 (do-tests "bv-ref-tests.rkt" "BV SDSL - Hacker's Delight synthesis")
 
