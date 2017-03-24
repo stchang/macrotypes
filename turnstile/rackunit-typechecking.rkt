28fa4dfb486734c7f4f5073c849e3b587f2a155a
Stephen Chang <stchang@ccs.neu.edu>
2017-03-24 10:59:52 -0400

    do not reprovide rackunit in rackunit-typechecking

--- turnstile/rackunit-typechecking.rkt
+++ turnstile/rackunit-typechecking.rkt
@@ -1,4 +1,4 @@
 #lang racket/base
-;; extends rackunit with type-checking forms
-(require rackunit "examples/tests/rackunit-typechecking.rkt")
-(provide (all-from-out rackunit "examples/tests/rackunit-typechecking.rkt"))
+;; extends some rackunit forms to test type-checking
+(require "examples/tests/rackunit-typechecking.rkt")
+(provide (all-from-out "examples/tests/rackunit-typechecking.rkt"))
