0795bb826dc4bb6c4eec751b647cd134141b7f3d
Stephen Chang <stchang@ccs.neu.edu>
2017-08-18 16:04:46 -0400

    indexed vectors basic example working

--- turnstile/examples/tests/dep-ind-list-tests.rkt
+++ turnstile/examples/tests/dep-ind-list-tests.rkt
@@ -67,19 +67,56 @@
 (check-type (len ((null Nat))) : Nat -> (Z))
 (check-type (len ((cons Nat) (Z) ((null Nat)))) : Nat -> (S (Z)))
 
-;; ;; "lists" parameterized over length
-;; (define-datatype Vect : (→ * Nat *)
-;;   [nil : (Π ([A : *][k : Nat]) (→ (Vect A (Z))))]
-;;   [cns : (Π ([A : *][k : Nat]) (→ A (Vect A k) (Vect A (S k))))])
+;; Vect: "lists" parameterized over length --------------------
+(define-datatype Vect : (→ * Nat *)
+  [nil : (Π ([A : *][k : Nat]) (→ (Vect A (Z))))]
+  [cns : (Π ([A : *][k : Nat]) (→ A (Vect A k) (Vect A (S k))))])
 
-;; (check-type nil : (Π ([A : *][k : Nat]) (→ (Vect A (Z)))))
-;; (check-type cns : (Π ([A : *][k : Nat]) (→ A (Vect A k) (Vect A (S k)))))
+(check-type nil : (Π ([A : *][k : Nat]) (→ (Vect A (Z)))))
+(check-type cns : (Π ([A : *][k : Nat]) (→ A (Vect A k) (Vect A (S k)))))
 
-;; (check-type (nil Nat (Z)) : (→ (Vect Nat (Z))))
-;; (check-type (cns Nat (Z)) : (→ Nat (Vect Nat (Z)) (Vect Nat (S (Z)))))
+(check-type (nil Nat (Z)) : (→ (Vect Nat (Z))))
+(check-type (cns Nat (Z)) : (→ Nat (Vect Nat (Z)) (Vect Nat (S (Z)))))
 
-;; (check-type ((nil Nat (Z))) : (Vect Nat (Z)))
-;; (check-type ((cns Nat (Z)) (Z) ((nil Nat (Z)))) : (Vect Nat (S (Z))))
+(check-type ((nil Nat (Z))) : (Vect Nat (Z)))
+(check-type ((cns Nat (Z)) (Z) ((nil Nat (Z)))) : (Vect Nat (S (Z))))
+(check-type ((cns Nat (S (Z))) (Z)
+             ((cns Nat (Z)) (Z)
+              ((nil Nat (Z)))))
+            : (Vect Nat (S (S (Z)))))
 
-;; (define-type-alias mtNatVec ((nil Nat (Z))))
-;; (check-type mtNatVec : (Vect Nat (Z)))
+(define-type-alias mtNatVec ((nil Nat (Z))))
+(check-type mtNatVec : (Vect Nat (Z)))
+
+(check-not-type (nil Nat (S (Z))) : (Vect Nat (S (Z))))
+
+;; length
+(check-type 
+ (elim-Vect ((nil Nat (Z)))
+            (λ ([A : *][k : Nat]) (λ ([v : (Vect A k)]) Nat))
+            (λ ([A : *][k : Nat]) (λ () (λ () (Z))))
+            (λ ([A : *][k : Nat])
+              (λ ([x : A][xs : (Vect A k)])
+                (λ ([IH : Nat])
+                  (S IH)))))
+ : Nat -> (Z))
+           
+(check-type
+ (elim-Vect ((cns Nat (Z)) (Z) ((nil Nat (Z))))
+            (λ ([A : *][k : Nat]) (λ ([v : (Vect A k)]) Nat))
+            (λ ([A : *][k : Nat]) (λ () (λ () (Z))))
+            (λ ([A : *][k : Nat])
+              (λ ([x : A][xs : (Vect A k)])
+                (λ ([IH : Nat])
+                  (S IH)))))
+ : Nat -> (S (Z)))
+           
+(check-type
+ (elim-Vect ((cns Nat (S (Z))) (Z) ((cns Nat (Z)) (Z) ((nil Nat (Z)))))
+            (λ ([A : *][k : Nat]) (λ ([v : (Vect A k)]) Nat))
+            (λ ([A : *][k : Nat]) (λ () (λ () (Z))))
+            (λ ([A : *][k : Nat])
+              (λ ([x : A][xs : (Vect A k)])
+                (λ ([IH : Nat])
+                  (S IH)))))
+ : Nat -> (S (S (Z))))
