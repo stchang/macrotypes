cffeb80c61953e374a407f71a4b8097b8bb0be54
Stephen Chang <stchang@ccs.neu.edu>
2018-05-03 10:38:38 -0400

    add some notes to sysf examples

--- macrotypes/examples/sysf.rkt
+++ macrotypes/examples/sysf.rkt
@@ -16,9 +16,14 @@
 (define-typed-syntax Λ
   [(_ (tv:id ...) e)
    #:with [tvs- e- τ-] (infer/ctx #'(tv ...) #'e)
+   ;; can't use internal mk-∀- constructor here
+   ;; - will cause the bound-id=? quirk to show up
+   ;;   (when subsequent tyvar refs are expanded with `type` stx class)
+   ;; - requires converting type= and subst to use free-id=?
+   ;;   (which is less performant)
    (⊢ e- : (∀ tvs- τ-))])
 (define-typed-syntax inst
   [(_ e τ:type ...)
    #:with [e- (~∀ tvs τ_body)] (infer+erase #'e)
-   (⊢ e- : #,(substs #'(τ.norm ...) #'tvs #'τ_body))]
+   [⊢ e- : #,(substs #'(τ.norm ...) #'tvs #'τ_body)]]
   [(_ e) #'e])
--- turnstile/examples/sysf.rkt
+++ turnstile/examples/sysf.rkt
@@ -16,6 +16,11 @@
 (define-typed-syntax (Λ (tv:id ...) e) ≫
   [[tv ≫ tv- :: #%type] ... ⊢ e ≫ e- ⇒ τ]
   --------
+  ;; can't use internal mk-∀- constructor here
+  ;; - will cause the bound-id=? quirk to show up
+  ;;   (when subsequent tyvar refs are expanded with `type` stx class)
+  ;; - requires converting type= and subst to use free-id=?
+  ;;   (which is less performant)
   [⊢ e- ⇒ (∀ (tv- ...) τ)])
 
 (define-typed-syntax inst
