b1bd926ca3f3b3720dfcbc046ed051f6235d65e0
Stephen Chang <stchang@ccs.neu.edu>
2018-10-09 20:04:11 -0400

    simplify simple/stlc.rkt

--- turnstile/examples/simple/stlc.rkt
+++ turnstile/examples/simple/stlc.rkt
@@ -9,16 +9,10 @@
   [(_ ([x:id : τ_in:type] ...) e) ≫
    [[x ≫ x- : τ_in.norm] ... ⊢ e ≫ e- ⇒ τ_out]
    -------
-   [⊢ (#%plain-lambda (x- ...) e-) ⇒ (→ τ_in.norm ... τ_out)]]
-  [(_ (x:id ...) e) ⇐ (~→ τ_in ... τ_out) ≫
-   [[x ≫ x- : τ_in] ... ⊢ e ≫ e- ⇐ τ_out]
-   ---------
-   [⊢ (#%plain-lambda (x- ...) e-)]])
+   [⊢ (#%plain-lambda (x- ...) e-) ⇒ (→ τ_in.norm ... τ_out)]])
 
 (define-typed-syntax (#%app e_fn e_arg ...) ≫
   [⊢ e_fn ≫ e_fn- ⇒ (~→ τ_in ... τ_out)]
-  #:fail-unless (stx-length=? #'[τ_in ...] #'[e_arg ...])
-                (num-args-fail-msg #'e_fn #'[τ_in ...] #'[e_arg ...])
   [⊢ e_arg ≫ e_arg- ⇐ τ_in] ...
   --------
   [⊢ (#%plain-app e_fn- e_arg- ...) ⇒ τ_out])
