92b146ba66e389791e6688b796afc6bfd7691ca6
Stephen Chang <stchang@ccs.neu.edu>
2018-10-17 13:40:17 -0400

    re-enable mlish tests

--- macrotypes-test/macrotypes/info.rkt
+++ macrotypes-test/macrotypes/info.rkt
@@ -1,3 +1,10 @@
 #lang info
+
 (define compile-omit-paths
   '("tests"))
+
+(define test-include-paths
+  '("tests/mlish")) ; to include .mlish files
+
+(define test-omit-paths
+  '("tests/mlish/bg/README.md"))
--- turnstile-test/turnstile/info.rkt
+++ turnstile-test/turnstile/info.rkt
@@ -2,3 +2,9 @@
 
 (define compile-omit-paths
   '("test"))
+
+(define test-include-paths
+  '("test/mlish")) ; to include .mlish files
+
+(define test-omit-paths
+  '("test/mlish/bg/README.md"))
