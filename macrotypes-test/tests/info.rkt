7070082dc0ea95c1ed2edb8397e5ce14e4f65943
Stephen Chang <stchang@ccs.neu.edu>
2018-10-17 15:56:28 -0400

    rearrange test pkg dirs from X/tests to tests/X

---
+++ macrotypes-test/tests/info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define compile-omit-paths
+  '("macrotypes"))
+
+(define test-include-paths
+  '("macrotypes/mlish")) ; to include .mlish files
+
+(define test-omit-paths
+  '("macrotypes/mlish/bg/README.md"))
---
+++ turnstile-test/tests/info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define compile-omit-paths
+  '("turnstile"))
+
+(define test-include-paths
+  '("turnstile/mlish")) ; to include .mlish files
+
+(define test-omit-paths
+  '("turnstile/mlish/bg/README.md"))
