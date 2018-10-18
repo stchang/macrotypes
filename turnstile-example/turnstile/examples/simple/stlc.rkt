f4812e07b6798866f4f34e1bc17c4d393193bead
Stephen Chang <stchang@ccs.neu.edu>
2018-10-18 11:59:47 -0400

    Update stlc.rkt
    single arity stlc example

--- turnstile-example/turnstile/examples/simple/stlc.rkt
+++ turnstile-example/turnstile/examples/simple/stlc.rkt
@@ -3,25 +3,23 @@
 
 (provide → λ #%app ann)
 
-(define-type-constructor → #:arity >= 1)
+(define-type-constructor →)
 
-(define-typed-syntax λ #:datum-literals (:)
-  [(_ ([x:id : τ_in:type] ...) e) ≫
-   [[x ≫ x- : τ_in.norm] ... ⊢ e ≫ e- ⇒ τ_out]
+(define-typed-syntax λ
+  [(_ [x:id (~datum :) τ_in:type] e) ≫
+   [[x ≫ x- : τ_in.norm] ⊢ e ≫ e- ⇒ τ_out]
    -------
-   [⊢ (#%plain-lambda (x- ...) e-) ⇒ (→ τ_in.norm ... τ_out)]]
-  [(_ (x:id ...) e) ⇐ (~→ τ_in ... τ_out) ≫
-   [[x ≫ x- : τ_in] ... ⊢ e ≫ e- ⇐ τ_out]
+   [⊢ (#%plain-lambda (x-) e-) ⇒ (→ τ_in.norm τ_out)]]
+  [(_ x:id e) ⇐ (~→ τ_in τ_out) ≫
+   [[x ≫ x- : τ_in] ⊢ e ≫ e- ⇐ τ_out]
    ---------
-   [⊢ (#%plain-lambda (x- ...) e-)]])
+   [⊢ (#%plain-lambda (x-) e-)]])
 
-(define-typed-syntax (#%app e_fn e_arg ...) ≫
-  [⊢ e_fn ≫ e_fn- ⇒ (~→ τ_in ... τ_out)]
-  #:fail-unless (stx-length=? #'[τ_in ...] #'[e_arg ...])
-                (num-args-fail-msg #'e_fn #'[τ_in ...] #'[e_arg ...])
-  [⊢ e_arg ≫ e_arg- ⇐ τ_in] ...
+(define-typed-syntax (#%app e_fn e_arg) ≫
+  [⊢ e_fn ≫ e_fn- ⇒ (~→ τ_in τ_out)]
+  [⊢ e_arg ≫ e_arg- ⇐ τ_in]
   --------
-  [⊢ (#%plain-app e_fn- e_arg- ...) ⇒ τ_out])
+  [⊢ (#%plain-app e_fn- e_arg-) ⇒ τ_out])
 
 (define-typed-syntax (ann e (~datum :) τ:type) ≫
   [⊢ e ≫ e- ⇐ τ.norm]
