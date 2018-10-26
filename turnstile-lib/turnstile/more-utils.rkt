f7f4b6222dd0f0ba17d8284c7e4296de45492829
Stephen Chang <stchang@ccs.neu.edu>
2018-10-26 14:09:24 -0400

    turnstile/typedefs: split out define-binding-type
    - streamlines defs and type representations
    - bump turnstile-lib version (for new type rep)

--- turnstile-lib/turnstile/more-utils.rkt
+++ turnstile-lib/turnstile/more-utils.rkt
@@ -2,25 +2,28 @@
 
 ;; turnstile library of extra stx helpers
 
-(require syntax/parse
-         (for-syntax racket/base syntax/parse racket/syntax))
+(provide (for-syntax x+τ stx-parse/fold)
+         define-nested/R define-nested/L)
 
-(provide x+τ λ/c- #;~plain-app/c define-nested/R define-nested/L stx-parse/fold)
+(begin-for-syntax
+  ;; syntax class matching [x : τ], ie a parameter with type annotation
+  ;; TODO: generalize to arbitrary tags (not always :)?
+  (define-syntax-class x+τ
+    (pattern [(~var x id) (~datum :) τ]))
 
-#;(define-syntax ~plain-app/c
-  (pattern-expander
-   (syntax-parser
-     ;       [_ #:do[(displayln (stx->datum this-syntax))] #:when #f #'debugging]
-     [(_ f) #'f]
-     [(_ f e (~and (~literal ...) ooo))
-      #'(~and TMP
-              (~parse (f e ooo)
-                      (stx-parse/fold #'TMP ((~literal #%plain-app) x xs))))]
-     [(_ f e . rst)
-      #'(~plain-app/c ((~literal #%plain-app) f e) . rst)])))
-
-(define-syntax-class x+τ
-  (pattern [(~var x id) (~datum :) τ]))
+  ;; returns a flattened list of stx objs, outermost first
+  ;; usage: (stx-parse/fold stx pattern)
+  ;; - where pattern has shape (pexpander element remainder)
+  (define-syntax stx-parse/fold ; foldl
+    (syntax-parser
+      [(_ e (pexpander x rst))
+       #:with L (generate-temporary 'L)
+       #:with e-rst (generate-temporary #'e)
+       #:with acc (generate-temporary 'acc)
+       #`(let L ([e-rst e][acc null])
+           (syntax-parse e-rst
+             [(pexpander x rst) (L #'rst (cons #'x acc))]
+             [last (reverse (cons #'last acc))]))])))
 
 ;; R = like foldr, eg λ
 ;; L = like foldl, eg app
@@ -36,8 +39,9 @@
          (wrap-fn ; eg pattern-expander
           (syntax-parser
             [(_ e) #'e]
-            [(_ x . rst) (syntax/loc this-syntax (name/1 x (name . rst)))]
-            )))]))
+            [(_ x . rst)
+             (quasisyntax/loc this-syntax
+               (name/1 x #,(syntax/loc this-syntax (name . rst))))])))]))
 (define-syntax define-nested/L
   (syntax-parser
     [(_ name:id name/1) #'(define-nested/L name name/1 #:as (λ (x) x))]
@@ -46,31 +50,6 @@
          (wrap-fn
           (syntax-parser
             [(_ e) #'e]
-            [(_ f e . rst) (quasisyntax/loc this-syntax (name #,(syntax/loc this-syntax (name/1 f e)) . rst))]
-            )))]))
-
-;; returns a flattened list of stx objs, outermost first
-;; usage: (stx-parse/fold stx pattern)
-;; - where pattern has shape (pexpander element remainder)
-(define-syntax stx-parse/fold ; foldl
-  (syntax-parser
-    [(_ e (pexpander x rst))
-     #:with L (generate-temporary 'L)
-     #:with e-rst (generate-temporary #'e)
-     #:with acc (generate-temporary 'acc)
-     #`(let L ([e-rst e][acc null])
-         (syntax-parse e-rst
-           [(pexpander x rst) (L #'rst (cons #'x acc))]
-           [last (reverse (cons #'last acc))]))]))
-
-;; untyped, curried
-(define-syntax (λ/c- stx)
-  (syntax-parse stx
-    [(_ () e) #'e]
-    [(_ (x . rst) e)
-     (syntax/loc stx (λ- (x) (λ/c- rst e)))]))
-
-
-           
-                       
-     
+            [(_ f e . rst)
+             (quasisyntax/loc this-syntax
+               (name #,(syntax/loc this-syntax (name/1 f e)) . rst))])))]))
