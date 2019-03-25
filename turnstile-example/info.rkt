f9d4100d42c7830c842fb981f0b4e348d192a00b
Stephen Chang <stchang@ccs.neu.edu>
2019-03-25 13:20:01 -0400

    turnstile/typedef: change current-resugar to resugar-type

--- turnstile-example/info.rkt
+++ turnstile-example/info.rkt
@@ -5,10 +5,10 @@
 (define deps
   '(("base" #:version "7.0")
     "typed-racket-lib"
-    ("turnstile-lib" #:version "0.4.2")
-    ("macrotypes-lib" #:version "0.3.2")))
+    ("turnstile-lib" #:version "0.4.9")
+    ("macrotypes-lib" #:version "0.3.3")))
 
 (define pkg-desc "Example languages for \"turnstile\".")
 (define pkg-authors '(stchang))
 
-(define version "0.4.2")
+(define version "0.4.3")
