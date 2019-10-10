4cbb76f6d2493b7816f8940ec5bdc73f4da89a9a
Stephen Chang <stchang@ccs.neu.edu>
2019-10-10 12:49:16 -0400

    fix invalid version formats

--- turnstile-example/info.rkt
+++ turnstile-example/info.rkt
@@ -5,10 +5,10 @@
 (define deps
   '(("base" #:version "7.0")
     "typed-racket-lib"
-    ("turnstile-lib" #:version "0.5.0")
+    ("turnstile-lib" #:version "0.5")
     ("macrotypes-lib" #:version "0.3.3")))
 
 (define pkg-desc "Example languages for \"turnstile\".")
 (define pkg-authors '(stchang))
 
-(define version "0.5.0")
+(define version "0.5")
